28c5135ee9e9b363a1c15a7d895cb7516505686c
AnonymousDonut <yuvalmasterglitch@gmail.com>
2024-11-20 20:02:11 +0200

    Add files via upload
    main commit

---
+++ DontJoinModdeds/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace DontJoinModdeds
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.donut.gorillatag.dontjoinmoddeds";
+        public const string Name = "DontJoinModdeds";
+        public const string Version = "1.0.0";
+    }
+}
